cc9cf0b984251496a22ef5a1a3e4a8591e41a714
Arthur-Escanor <64905903+Arthur-Escanor@users.noreply.github.com>
2020-07-03 19:24:56 -0500

    preparando proyecto

---
+++ views/contenido.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link  href= 'https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar Sesion</a>
+        <hr class="border">
+        <div class="contenido">
+           <article>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Nesciunt enim explicabo necessitatibus iste maxime aliquam molestias amet eveniet.
+              Vitae fuga possimus dolores dolorem consectetur vel fugit nobis earum molestiae omnis.</p>
+        
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Nesciunt enim explicabo necessitatibus iste maxime aliquam molestias amet eveniet.
+               Vitae fuga possimus dolores dolorem consectetur vel fugit nobis earum molestiae omnis.</p>
+            </article>
+        </div>    
+    </div>
+</body>
+</html>
